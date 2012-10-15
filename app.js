ba9a368cc535aa7a3728886c04b8b57abebc6c83
teemujonkkari <teemu.jonkkari@gmail.com>
2012-10-15 14:03:56 +0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,49 @@
+// http://github.com/nodejitsu/node-http-proxy
+
+// Libs
+var fs = require('fs'),
+	http = require('http'),
+	https = require('https'),
+	httpProxy = require('http-proxy');
+
+// Server Instance
+var server = http.createServer(function (req, res) {
+	res.writeHead(200, { 'Content-Type': 'text/plain' });
+	res.write('Proxy says hello o/ !' + '\n' );
+	res.end();
+}).listen(9000);
+
+// Proxy Settings
+var optionsHTTPS = {
+	https: {
+		key: fs.readFileSync('/etc/apache2/ssl/amc.key.pem', 'utf8'),
+		cert: fs.readFileSync('/etc/apache2/ssl/amc.crt.pem', 'utf8')
+	},
+	hostNameOnly: true,
+	router: {
+		'amc2.pori.tut.fi': '127.0.0.1:8080',
+		'amc.pori.tut.fi/game-server/': '127.0.0.1:9002',
+		'amc.pori.tut.fi': '127.0.0.1:8080',
+		'magos.pori.tut.fi/editor/': '127.0.0.1:9001',
+		'magos.pori.tut.fi/play/': '127.0.0.1:9003',
+		'magos.pori.tut.fi': '127.0.0.1:8081'
+	}
+};
+
+var optionsHTTP = {
+	hostNameOnly: true,
+	router: {
+		'amc2.pori.tut.fi': '127.0.0.1:8080', // amc2 www-server
+		'amc.pori.tut.fi/game-server/': '127.0.0.1:9002', // game socket server
+		'amc.pori.tut.fi': '127.0.0.1:8080', // amc www-server
+		'magos.pori.tut.fi/editor/': '127.0.0.1:9001', // magos editor
+		'magos.pori.tut.fi/play/': '127.0.0.1:9003', // magos game platform
+		'magos.pori.tut.fi': '127.0.0.1:8081' // django
+	}
+};
+
+var proxyHTTPServer = httpProxy.createServer(optionsHTTP);
+proxyHTTPServer.listen(80);
+
+var proxyHTTPSServer = httpProxy.createServer(optionsHTTPS);
+proxyHTTPSServer.listen(443);
